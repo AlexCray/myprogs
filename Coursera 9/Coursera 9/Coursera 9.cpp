fef2294620862dfa458fc0cf5da89df1ed24ac09
Alexander <72artes@mail.ru>
2020-07-29 20:02:57 +0500

    I've done some exercises

---
+++ Coursera 9/Coursera 9/Coursera 9.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> v;
+    while (N >= 2)
+    {
+        v.push_back(N % 2);
+        N /= 2;
+    }
+    v.push_back(N);
+   
+    for (int i=v.size(); i>0; i--)
+    {
+        cout << v[i-1];
+    } 
+}
---
+++ Coursera7/Coursera7/Coursera7.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+
+    int i{ 0 }, N{ 0 };
+    for (char c : S)
+    {
+        if (c == 'f')
+        {
+            if (N == 1)
+            {
+                cout << i;
+            }
+            N++;
+        }
+        i++;
+    }
+
+    if (N == 1) 
+    {
+        cout << -1;
+    }
+
+    if (N == 0)
+    {
+        cout << -2;
+    }
+}
